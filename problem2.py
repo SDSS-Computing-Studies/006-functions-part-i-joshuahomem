5853ee27d0807eec32442593f3abb050901cb4ca
joshuahomem <joshuah677@deltalearns.ca>
2020-10-08 12:24:42 -0700

    new update

--- problem2.py
+++ problem2.py
@@ -8,4 +8,16 @@ Note that the coordinates should be signed (positive or negative) floats
 (2 points)
 """
 import math
-def distance(x,y)
+def distance(x,y):
+    one=x[0]
+    two=x[1]
+    three=y[0]
+    four=y[1]
+    dist1=one-three
+    dist2=two-four
+    acdist=(dist1**2 + dist2**2)**(1/2)
+    acdist=round(acdist,3)
+    return acdist
+
+p=distance((1,1),(2,2))
+print(p)
--- problem3.py
+++ problem3.py
@@ -8,3 +8,12 @@ into another number.
 Example: The factors of 10 are 1, 2, 5, 10
 (2 points)
 """
+def factors(inp):
+    a=[]
+    for i in range(1,inp+1):
+        if inp%i==0:
+            a.append(i)
+    a.sort()
+    return a
+print(factors(24))
+
